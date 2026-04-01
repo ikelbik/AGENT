253c4bcab5a0bc2fe82ebbdc667735a829cf3cc0
ikelbik <144374840+ikelbik@users.noreply.github.com>
2026-04-02 00:07:16 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
 import 'dotenv/config'
-import { createBot }     from './bot/telegram.js'
 import { startApiServer } from './server/api.js'
 
-async function main() {
-  console.log('Starting AgentNet...')
-
-  startApiServer()
-
-  const bot = createBot()
-
-  process.once('SIGINT',  () => bot.stop())
-  process.once('SIGTERM', () => bot.stop())
-
-  await bot.start({
-    drop_pending_updates: true,
-    onStart: (info) => {
-      console.log(`Bot @${info.username} started`)
-    }
-  })
-}
-
-main().catch(console.error)
+startApiServer()
